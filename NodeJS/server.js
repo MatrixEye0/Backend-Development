70bed9422106bc7fc8eaeb460774998d781d8019
matrixeye0 <singhjagjit9735@gmail.com>
2026-09-01 19:52:22 +0530

    url handaling

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,24 +1,33 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 // create server ----------------------------------------------------------------------------------------------------
 const myServer = http.createServer((request, response )=>{
-    console.log(request.headers); // request header detail 
+    // console.log(request.headers); // request header detail 
 
     const log = `Today date : ${Date.now()} : ${request.url}\n` // server restart by ct+c in terminal and refresh lochalhost:port
-    fs.appendFile('log.txt',log,(e,data)=>{ // accept request in file
+    const myurl =url.parse(request.url,true) // parse url and query string
+    console.log(myurl)
+
+    fs.appendFile('log.txt',log,(error,data)=>{ // accept request in file
           // response.end("I Accept your request");
-          switch(request.url){
+
+          // switch(request.url)
+          switch(myurl.pathname)
+          {
             case '/home': response.end('Welcome to Home Page Jagjit')
             break;
-            case '/about': response.end('Welcome to About Page Jagjit')
+            case '/about': 
+            const user = myurl.query.myname; // query string
+            response.end(`${user} Welcome to About Page Jagjit`) // http://localhost:8000/about?myname=Jagjit give query string in url
+
             break;
             case '/contact': response.end('Welcome to Contact Page Jagjit')
             break;
             default : response.end('Ooops page not found  !!!!');
           }
     })
-
     // response.end("I Accept your request")
 });
 
