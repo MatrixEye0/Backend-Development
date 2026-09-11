d7b00a92d8de0a27c4386e6cd164755930de37ec
matrixeye0 <singhjagjit9735@gmail.com>
2026-09-11 23:03:29 +0530

    rete limit and helmet

--- CORS/index.js
+++ CORS/index.js
@@ -3,3 +3,25 @@
 // origin = protocol + domain + port
 // suppose i have api https://api.myapp.com and frontend in https://myapp.com Your backend can tell the browser: I allow requests from https://myapp.com.
 // in cors we allow which access api or which not
+
+// const express = require("express");
+// const helmet = require("helmet");
+// const rateLimit = require("express-rate-limit");
+// const cors = require("cors");
+
+// const app = express();
+
+// app.use(helmet());
+
+// app.use(cors({
+//     origin: "http://localhost:5173"
+// }));
+
+// app.use(express.json());
+
+// const apiLimiter = rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 100
+// });
+
+// app.use("/api", apiLimiter);
