71e34bbc9b0f50237501536917dc1c591c8fa24f
matrixeye0 <singhjagjit9735@gmail.com>
2026-09-11 16:22:26 +0530

    cors defination

---
+++ CORS/index.js
@@ -0,0 +1,5 @@
+// CORS = Cross-Origin Resource Sharing.
+// It is a browser security mechanism that controls whether a frontend running on one origin is allowed to call an API running on another origin.
+// origin = protocol + domain + port
+// suppose i have api https://api.myapp.com and frontend in https://myapp.com Your backend can tell the browser: I allow requests from https://myapp.com.
+// in cors we allow which access api or which not
